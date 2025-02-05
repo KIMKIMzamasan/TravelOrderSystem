2a1d9e78cda19f54127b1ac1f61cb52a59b12b85
KIMKIMzamasan <ckimkimsama@gmail.com>
2025-02-05 13:24:59 +0800

    added functionality to logout button

--- TravelOrderSystem/Pages/Admin.xaml.vb
+++ TravelOrderSystem/Pages/Admin.xaml.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Admin
 
+    Private Sub Mainform_FormClosed(sender As Object, e As EventArgs) Handles Me.Closed
+        Application.Current.Shutdown()
+    End Sub
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        ShowMainWindow()
+    End Sub
+    Private Sub ShowMainWindow()
+        Dim mainWindow As New MainWindow()
+        mainWindow.Show()
+
+        Me.Hide()
+    End Sub
 End Class
