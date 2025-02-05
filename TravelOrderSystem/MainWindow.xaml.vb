530de3963d667d895b16025b4700dc5b5792b605
KIMKIMzamasan <ckimkimsama@gmail.com>
2025-02-05 11:58:21 +0800

    added another form /Admin

--- TravelOrderSystem/MainWindow.xaml.vb
+++ TravelOrderSystem/MainWindow.xaml.vb
@@ -1,3 +1,18 @@
 ﻿Class MainWindow
 
+    Private Sub MainWindow_FormClosed(sender As Object, e As EventArgs) Handles Me.Closed
+        Application.Current.Shutdown()
+
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        ShowAdminWindow()
+    End Sub
+
+    Private Sub ShowAdminWindow()
+        Dim adminWindow As New Admin()
+        adminWindow.Show()
+
+        Me.Hide()
+    End Sub
 End Class
